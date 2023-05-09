6e74c152708f18669de03d34026dcc8cdd996b81
Nycollas <sobolevski.academy@gmail.com>
2023-05-09 11:40:41 -0300

    UPDATE: js na pagina main e começo de mudanca de tema

---
+++ Main/main.js
@@ -0,0 +1,7 @@
+function options() {
+    var OcultedContent = document.getElementById("oculted-container");
+    if(OcultedContent.style.display == 'flex')
+        OcultedContent.style.display = 'none';
+    else
+        OcultedContent.style.display = 'flex'
+}
